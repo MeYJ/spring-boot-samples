9cabb86151bbc006e8c92e7fc9965a6bcf044bc6
MeYJ <me.yangjun@gmail.com>
2018-06-20 18:01:06 +0800

    add spring-boot-webapp

---
+++ spring-boot-webapp/src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by MeYJ on 2018/6/7.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
